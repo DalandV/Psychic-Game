d6a6f1f78df4c6c99e1526feafad9e78a58e7e05
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2018-08-15 20:44:31 -0400

    Fixed It

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,19 +2,61 @@
 // VARIABLES
 // ---------------------------------
 
-var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-console.log(computerChoices)
+// Computer chooses random letter
+var computerOptions = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-// Start with 0 Wins
-var winsScore = document.getElementById('wins').innerHTML = "Wins: " + 0;
+// var computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+// console.log(computerChoice)
 
-// Start with 0 Losses
-var lossScore = document.getElementById('losses').innerHTML = "Lossed: " + 0;
+// User starts with 0 Wins
+var winsScore = 0;
 
-// Start with 9 Guesses Left
-var guessesLeft = document.getElementById('guesses-left').innerHTML = "Guesses Left: " + 9;
+// User starts with 0 Losses
+var lossScore = 0;
 
-// Store Guesses so far
-var guessesSoFar = document.getElementById('guesses-so-far').innerHTML = "Your Guesses so far: ";
+// User starts with 9 Guesses Left
+var guessesLeft = 9;
 
+// Store Users Guesses so far
+var guessesSoFar = [];
+
+// FUNCTION
+// ---------------------------------
+
+document.onkeyup = function (event) {
+
+    var computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+    console.log(computerChoice)
+
+    var userGuess = event.key;
+    console.log(userGuess)
+
+    if (userGuess === computerChoice) {
+        console.log("Success!!");
+        winsScore++;
+        console.log(winsScore);
+        guessesLeft = 9;
+        guessesSoFar = [];
+
+    }
+    else if (userGuess !== computerChoice) {
+        console.log("Wrong Letter!");
+        guessesLeft--;
+        guessesSoFar.push(userGuess);
+    }
+    if (guessesLeft === 0 ) {
+        guessesLeft = 9;
+        lossScore++;
+        guessesSoFar = [];
+    }
+    var scoreBoard =
+
+            "<h1>The Psychic Game</h1>" +
+            "<p>Guess what letter I'm thinking of</p>" +
+            "<p>Wins: " + winsScore + "</p>" +
+            "<p>Losses: " + lossScore + "</p>" +
+            "<p>Guesses Left: " + guessesLeft + "</p>" +
+            "<p>Guesses So Far: " + guessesSoFar.join(", ") + "</p>";
+
+        document.querySelector("#game").innerHTML = scoreBoard;
+}
