d8494c9316b93f0debc48aca72662acb50f861de
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2019-01-25 09:24:36 -0500

    Replaced vars with let and const

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,93 @@
-
 // VARIABLES
 // ---------------------------------
 
 // Computer chooses random letter
-var computerOptions = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-// var computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-// console.log(computerChoice)
+const computerOptions = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
 
 // User starts with 0 Wins
-var winsScore = 0;
+let winsScore = 0;
 
 // User starts with 0 Losses
-var lossScore = 0;
+let lossScore = 0;
 
 // User starts with 9 Guesses Left
-var guessesLeft = 9;
+let guessesLeft = 9;
 
 // Store Users Guesses so far
-var guessesSoFar = [];
+let guessesSoFar = [];
 
-// FUNCTION
+// FUNCTIONS
 // ---------------------------------
 
-document.onkeyup = function (event) {
+document.onkeyup = function(event) {
+  const computerChoice =
+    computerOptions[Math.floor(Math.random() * computerOptions.length)];
+  console.log(computerChoice);
 
-    var computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
-    console.log(computerChoice)
+  const userGuess = event.key;
+  console.log(userGuess);
 
-    var userGuess = event.key;
-    console.log(userGuess)
-
-    if (computerOptions.indexOf(userGuess) > -1) {
-        if (userGuess === computerChoice) {
-            console.log("Success!!");
-            winsScore++;
-            console.log(winsScore);
-            guessesLeft = 9;
-            guessesSoFar = [];
-
-        }
-        else if (userGuess !== computerChoice) {
-            console.log("Wrong Letter!");
-            guessesLeft--;
-            guessesSoFar.push(userGuess);
-        }
-        if (guessesLeft === 0) {
-            guessesLeft = 9;
-            lossScore++;
-            guessesSoFar = [];
-        }
+  if (computerOptions.indexOf(userGuess) > -1) {
+    if (userGuess === computerChoice) {
+      console.log("Success!!");
+      winsScore++;
+      console.log(winsScore);
+      guessesLeft = 9;
+      guessesSoFar = [];
+    } else if (userGuess !== computerChoice) {
+      console.log("Wrong Letter!");
+      guessesLeft--;
+      guessesSoFar.push(userGuess);
     }
-    
-    var scoreBoard =
+    if (guessesLeft === 0) {
+      guessesLeft = 9;
+      lossScore++;
+      guessesSoFar = [];
+    }
+  }
 
-        "<h1>The Psychic Game</h1>" +
-        "<p>Guess what letter I'm thinking of</p>" +
-        "<p>Wins: " + winsScore + "</p>" +
-        "<p>Losses: " + lossScore + "</p>" +
-        "<p>Guesses Left: " + guessesLeft + "</p>" +
-        "<p>Guesses So Far: " + guessesSoFar.join(", ") + "</p>";
+  const scoreBoard =
+    "<h1>The Psychic Game</h1>" +
+    "<p>Guess what letter I'm thinking of</p>" +
+    "<p>Wins: " +
+    winsScore +
+    "</p>" +
+    "<p>Losses: " +
+    lossScore +
+    "</p>" +
+    "<p>Guesses Left: " +
+    guessesLeft +
+    "</p>" +
+    "<p>Guesses So Far: " +
+    guessesSoFar.join(", ") +
+    "</p>";
 
-    document.querySelector("#game").innerHTML = scoreBoard;
-}
+  document.querySelector("#game").innerHTML = scoreBoard;
+};
