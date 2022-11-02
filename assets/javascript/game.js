8965a02d3534da64cd0ad226d6d7f658863c8bf4
Daland Dixon <39280836+DalandV@users.noreply.github.com>
2022-11-02 12:52:34 -0400

    Update Notes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 // VARIABLES
 // ---------------------------------
 
-// Computer chooses random letter
+// Options the computer will choose from
 const computerOptions = [
   "a",
   "b",
@@ -32,6 +32,8 @@ const computerOptions = [
   "z",
 ];
 
+// Add "computerChoice" variable
+
 // User starts with 0 Wins
 let winsScore = 0;
 
@@ -48,32 +50,58 @@ let guessesSoFar = [];
 // ---------------------------------
 
 document.onkeyup = function (event) {
+  //When the user presses a key
   const computerChoice =
     computerOptions[Math.floor(Math.random() * computerOptions.length)];
+  // ** Changes the computer choice on each guess instead of each round **
   console.log(computerChoice);
 
   const userGuess = event.key;
   console.log(userGuess);
 
   if (computerOptions.indexOf(userGuess) > -1) {
+    // Only triggers when a user presses a letter key
+
     if (userGuess === computerChoice) {
       console.log("Success!!");
+
       winsScore++;
+      // On a correct guess the user gets 1 point added to their "Wins" Score
       console.log(winsScore);
+
+      // Add code to update "Wins" score using html file
+
+      // ***************
       guessesLeft = 9;
       guessesSoFar = [];
+      // ***************
+      // ** Repeated Code: This should be a function **
+      // And a new round begins
     } else if (userGuess !== computerChoice) {
       console.log("Wrong Letter!");
       guessesLeft--;
+      // Their chance to guess is reduced by 1
+
       guessesSoFar.push(userGuess);
+      // And the incorrect guess is added to the "guessesSoFar" array
     }
     if (guessesLeft === 0) {
-      guessesLeft = 9;
+      // If the user runs out of guesses
+
       lossScore++;
+      // 1 point is added to their "Losses" Score
+
+      // Add code to update "Losses" score using html file
+
+      // ***************
+      guessesLeft = 9;
       guessesSoFar = [];
+      // ***************
+      // ** Repeated Code: This should be a function **
     }
   }
 
+  // *********************************************************
   const scoreBoard = `<h1>The Psychic Game</h1>
     <p>Guess what letter I'm thinking of</p>
     <p>Wins: ${winsScore} </p>
@@ -82,4 +110,13 @@ document.onkeyup = function (event) {
     <p>Guesses So Far: ${guessesSoFar.join(", ")} </p>`;
 
   document.querySelector("#game").innerHTML = scoreBoard;
+  // *********************************************************
+  // Repeats code from html file ** Get rid of this **
+
+  /*   
+  Add code to set up a new round:
+  "Guesses Left:"
+  "Guesses So Far:"
+  and "computerChoice" should be reset together
+   */
 };
